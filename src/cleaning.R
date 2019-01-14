e53ef25dcdd35b02624e49ea4bd42d1daec518dc
Alden Chen <alden.chen@outlook.com>
2019-01-14 14:55:32 -0800

    Reshape to tidy format

--- src/cleaning.R
+++ src/cleaning.R
@@ -4,12 +4,15 @@ data <- read_csv("data/ucr_crime_1975_2015.csv")
 
 data <- data %>% 
   select(city = department_name,
-         total_violent_crime = violent_per_100k,
-         rape = rape_per_100k,
-         homicide = homs_per_100k,
-         robbery = rob_per_100k,
-         aggravated_assault = agg_ass_per_100k,
-         year)
+         `Total Violent Crime` = violent_per_100k,
+         Rape = rape_per_100k,
+         Homicide = homs_per_100k,
+         Robbery = rob_per_100k,
+         `Aggravated Assault` = agg_ass_per_100k,
+         year) %>% 
+  gather(key = "type", value = "n", 2:6 )
 
 write_csv(data, "data/cleaned_data.csv")
+
+
   
